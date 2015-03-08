605eedd7bfe84b59533c21ebd8a47897bd05b7e0
Paul M. Jones <pmjones88@gmail.com>
2015-03-08 12:18:32 -0500

    use the container to get the command

--- src/Command.php
+++ src/Command.php
@@ -1,29 +1,53 @@
 <?php
 namespace Bookdown\Apidown;
 
+use Aura\Cli\Context;
+use Aura\Cli\Stdio;
+
 class Command
 {
-    protected $file;
+    protected $context;
+    protected $collector;
 
-    public function __construct($argv)
-    {
-        if (! isset($argv[1])) {
-            echo "Pass a structure.xml path as the first argument.";
-            return 1;
-        }
-        $this->file = $argv[1];
+    public function __construct(
+        Context $context,
+        Stdio $stdio,
+        Collector $collector
+    ) {
+        $this->context = $context;
+        $this->stdio = $stdio;
+        $this->collector = $collector;
     }
 
     public function __invoke()
     {
-        echo "Parsing {$this->file}" . PHP_EOL;
-        $xml = simplexml_load_file($this->file);
-        $parser = new Collector(new Builder);
-        $classes = $parser($xml);
+        try {
+            $file = $this->init();
+            $classes = $this->collector->__invoke($file);
+            $this->dump($classes);
+            return 0;
+        } catch (AnyException $e) {
+            $this->stdio->errln($e->getMessage());
+            $code = $e->getCode() ? $e->getCode() : 1;
+            return $code;
+        }
+    }
 
-        $output = var_export($classes, true);
-        $output = preg_replace("/\=\>\s*\n\s*/m", "=> ", $output);
-        echo $output;
-        return 0;
+    protected function init()
+    {
+        $file = $this->context->argv->get(1);
+        if (! $file) {
+            throw new Exception(
+                "Please enter the path to a structure.xml file as the first argument."
+            );
+        }
+        return $file;
+    }
+
+    protected function dump($classes)
+    {
+        $export = var_export($classes, true);
+        $export = preg_replace("/\=\>\s*\n\s*/m", "=> ", $export);
+        $this->stdio->outln($export);
     }
 }

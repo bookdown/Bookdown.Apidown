18366a044e3b1ab8fc506498dfb01cfb2e533034
Paul M. Jones <pmjones88@gmail.com>
2015-03-08 12:18:07 -0500

    use a container for objects

---
+++ src/Container.php
@@ -0,0 +1,71 @@
+<?php
+namespace Bookdown\Apidown;
+
+use Aura\Cli\CliFactory;
+
+class Container
+{
+    protected $stdout;
+    protected $stderr;
+    protected $stdio;
+    protected $cliFactory;
+    protected $fsioClass;
+    protected $fsio;
+
+    public function __construct(
+        $stdout = 'php://stdout',
+        $stderr = 'php://stderr',
+        $fsioClass = 'Bookdown\Bookdown\Fsio'
+    ) {
+        $this->stdout = $stdout;
+        $this->stderr = $stderr;
+        $this->fsioClass = $fsioClass;
+    }
+
+    public function newCommand($globals)
+    {
+        return new Command(
+            $this->getCliFactory()->newContext($globals),
+            $this->getStdio(),
+            $this->newCollector()
+        );
+    }
+
+    public function newCollector()
+    {
+        return new Collector(
+            $this->getStdio(),
+            $this->getFsio(),
+            new Builder()
+        );
+    }
+
+    public function getCliFactory()
+    {
+        if (! $this->cliFactory) {
+            $this->cliFactory = new CliFactory();
+        }
+        return $this->cliFactory;
+    }
+
+    public function getStdio()
+    {
+        if (! $this->stdio) {
+            $this->stdio = $this->getCliFactory()->newStdio(
+                'php://stdin',
+                $this->stdout,
+                $this->stderr
+            );
+        }
+        return $this->stdio;
+    }
+
+    public function getFsio()
+    {
+        if (! $this->fsio) {
+            $class = $this->fsioClass;
+            $this->fsio = new $class();
+        }
+        return $this->fsio;
+    }
+}
